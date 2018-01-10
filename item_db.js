8c5e03562a780aac42a13510f78065292640c552
Sroubek <honza.sroubek@seznam.cz>
2018-01-10 12:15:09 +0100

    Added /login and /profile

--- item_db.js
+++ item_db.js
@@ -3,11 +3,35 @@
 /*eslint-env node*/
 const alasql = require('alasql');
 
-module.exports = alasql('CREATE TABLE users (userId INT PRIMARY KEY AUTOINCREMENT, username varchar(255), password varchar(255))');
-
-module.exports = alasql('CREATE TABLE items (itemId INT PRIMARY KEY AUTOINCREMENT,userId INT REFERENCES users(userId), name varchar(255), quantity int, unit varchar(25))');
+alasql('CREATE TABLE users (userId INT PRIMARY KEY AUTOINCREMENT, username varchar(255), password varchar(255))');
+alasql('CREATE TABLE items (itemId INT PRIMARY KEY AUTOINCREMENT,userId INT REFERENCES users(userId), name varchar(255), quantity int, unit varchar(25))');
 
 
 alasql('INSERT INTO users VALUES (\'\',\'Screwee\',\'Password01\')');
 alasql('INSERT INTO users VALUES (\'\',\'TanakhT\',\'Cookies58\')');
 alasql('INSERT INTO users VALUES (\'\',\'VZV\',\'Password02\')');
+
+var records = alasql('SELECT * FROM users');
+
+exports.findById = function(id, cb) {
+	process.nextTick(function() {
+		var idx = id - 1;
+		if (records[idx]) {
+			cb(null, records[idx]);
+		} else {
+			cb(new Error('User ' + id + ' does not exist'));
+		}
+	});
+};
+
+exports.findByUsername = function(username, cb) {
+	process.nextTick(function() {
+		for (var i = 0, len = records.length; i < len; i++) {
+			var record = records[i];
+			if (record.username === username) {
+				return cb(false, record);
+			}
+		}
+		return cb(true, null);
+	});
+};
--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,57 @@
 
 const express = require('express');
 const router = express.Router();
-
 const alasql = require('alasql');
 require('../item_db.js');
+const db = require('../item_db.js');
+const passport = require('passport');
+const JsonStrategy = require('passport-json').Strategy;
+const bodyParser = require('body-parser');
 
-// Add your code here:
+// logging, parsing, and session handling.
+router.use(require('morgan')('combined'));
+router.use(require('cookie-parser')());
+//app.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+router.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+
+passport.use(new JsonStrategy(
+	function(username, password, cb) {
+		db.findByUsername(username, function(err, user) {
+			if (err || !user) {
+				return cb(true, null);
+			}
+			if (user.password != password) {
+				return cb(true, null);
+			}
+
+			return cb(false, user);
+		});
+	}));
+
+passport.serializeUser(function(user, cb) {
+	cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+	db.findById(id, function (err, user) {
+		if (err) { return cb(err); }
+		cb(null, user);
+	});
+});
+
+function isLoggedIn(req, res, next) {
+	if(req.isAuthenticated()) {
+		return next();
+	}
+	return res.status(403).send('You are not logged in');
+	//return res.redirect('/login');
+}
+
+router.use(passport.initialize());
+router.use(passport.session());
+
+
 router.param('itemId', (req, res, next, id) => {
 	const itemId = Number(id);
 	if ( isNaN(itemId)){
@@ -29,6 +75,13 @@ router.param('itemId', (req, res, next, id) => {
 router.get('/items/', (req, res, next) => {
 	const items = alasql('SELECT * FROM items');
 	res.send(items);
+	next();
+});
+
+// Get all Users
+router.get('/users/', (req, res, next) => {
+	const users = alasql('SELECT * FROM users');
+	res.send(users);
 	next();
 });
 
@@ -62,6 +115,19 @@ router.delete('/items/', (req, res, next) => {
 	alasql('DELETE FROM items');
 	res.status(204).send();
 	next();
+});
+
+router.post('/login', function(req, res, next) {
+	passport.authenticate('json', function(err, user) {
+		if (err) {
+			return res.status(403).send('Invalid username or password');
+		}
+		return res.status(200).send('Hello user ' + user.username);
+	})(req, res, next);
+});
+
+router.get('/profile', isLoggedIn, function(req, res){
+	return res.status(200).send(req.user.username);
 });
 
 module.exports = router;
